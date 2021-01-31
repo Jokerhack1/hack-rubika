0b9fe644bc3a16d47e622434b123738f6f701fa1
Jokerhack <76730200+Jokerhack1@users.noreply.github.com>
2021-01-31 12:29:06 +0330

    Update hack-rubika.py

--- hack-rubika.py
+++ hack-rubika.py
@@ -15,9 +15,9 @@ print("|             @jokerhack_channel  |")
 print("###################################")
 
 
-hack = input("TARGET  PHONE : ")
+hack = input("TARGET  PHONE or ID : ")
 
-l = ["hacking the rubika 18%...", "hacking the rubika 32%....", "hacking the rubika 58%.....", "hacking the rubika 76%......", "hacking the rubika 99%.......", " * hacking failed - Done! Crack the code  rubika target :* "]
+l = ["hacking the rubika 18%...", "hacking the rubika 32%....", "hacking the rubika 58%.....", "hacking the rubika 76%......", "hacking the rubika 99%.......", " * hacking  - Done! Crack the code  rubika target :* "]
 
 for i in l:
     print("\r" + i, end="")
